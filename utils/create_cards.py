27fbf2a657e26931c8263f453f7de910b8255ad7
francesco <vignif@gmail.com>
2020-11-04 23:49:05 +0100

    Update folder name

---
+++ utils/create_cards.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw
+folder = './images/'
+
+def create_it(num):
+    img = Image.new('RGB', (25, 35), color='white')
+    d = ImageDraw.Draw(img)
+    d.text((5,10), str(num), fill=(0,0,0))
+    img.save(folder+str(num)+'.png')
+
+if __name__ == '__main__':
+    for i in range(0,101):
+        create_it(i)
